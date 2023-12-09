c8758bab6ca849e284f0aad54c110e254493c2cc
akshatgg <akshatg9636@gmail.com>
2023-12-10 04:31:35 +0545

    error solved for hosting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss')} */
 export const content = ["./src/**/*.{js,jsx}"];
 export const mode = "jit";
 export const theme = {
