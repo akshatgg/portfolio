723451ca89a5f2f5a2bc5aa44dd58a09ccc1a6da
akshatgg <akshatg9636@gmail.com>
2023-12-14 21:19:14 +0545

    error comes in bg

--- welcome/src/App.jsx
+++ welcome/src/App.jsx
@@ -1,12 +1,14 @@
-
+import Particlesbg from "./components/Particlesbg";
 
 function App() {
-
   return (
-    <div >
-   hello 
+    <div className="App">
+      <Particlesbg />
+      <div className="particlesheader">
+        <h1>Particles.js</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ welcome/src/components/Particlesbg.jsx
@@ -0,0 +1,11 @@
+import Particles from "react-tsparticles"
+import particlesConfig from "./config/particles.config"
+function Particlesbg() {
+  return (
+    <Particles params={particlesConfig}> 
+
+    </Particles>
+  )
+}
+
+export default Particlesbg
