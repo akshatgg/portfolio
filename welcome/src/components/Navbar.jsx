f6d6ba7c7b8d15abcac9c9c4a0cd13e714e85146
akshatgg <akshatg9636@gmail.com>
2023-12-16 18:27:44 +0545

    every property is working

--- welcome/src/components/Navbar.jsx
+++ welcome/src/components/Navbar.jsx
@@ -1,25 +1,61 @@
+// import React from 'react';
 import { Link } from 'react-router-dom';
-
+import profile from '../assets/profile.png';
+import contact from '../assets/contact.png';
+import book from '../assets/book.png';
+import home from '../assets/home.png';
+import tech from '../assets/technical-support.png';
+import './Navbar.css'; 
 function Navbar() {
-  return (
-    <ul className="grid grid-rows-1 grid-flow-col">
-      <li className='text-white'>
-        <Link to="/home" >About</Link>
-      </li>
-      <li className='text-white'>
-        <Link to="/home" >Contact</Link>
-      </li>
-      <li className='text-white'>
-        <Link to="/home" >Home</Link>
-      </li>
-      <li className='text-white'>
-        <Link to="/home" >Project</Link>
-      </li>
-      <li className='text-white'>
-        <Link to="/home" >Service</Link>
-      </li>
 
-    </ul>
+  const iconColor = '#3498db'; 
+  const imageStyle = {
+    filter: 'invert(1) ',
+            height: '24px', // Adjust the height as needed
+            color: iconColor,
+  };
+
+
+  return (
+    <div className="flex justify-center">
+      <ul className="grid grid-rows-1 grid-col-13 gap-11 mt-4">
+        <li className='text-white col-start-1 flex flex-col items-center'>
+          <Link to="/About" className='hover:text-blue-300 transition duration-500'>
+          <img
+  src={profile}
+  alt='About'
+  style={imageStyle}
+  className={({isActive})=> `${isActive ? 'text-blue-300' : 'text-white'}` }
+/>            About
+          </Link>
+        </li>
+        <li className='text-white col-start-2 flex flex-col items-center'>
+          <Link to="/Contact" className='hover:text-blue-300 transition duration-500'>
+            <img src={contact} alt='Contact' style={imageStyle} className={({isActive})=> `${isActive ? 'text-blue-300' : 'text-white'}` }/>
+            Contact
+          </Link>
+        </li>
+        <li className='text-white col-start-3 flex flex-col items-center'>
+          <Link to="/" className='hover:text-blue-300 transition duration-500'>
+            <img src={home} alt='Home' style={imageStyle} className={({isActive})=> `${isActive ? 'text-blue-300' : 'text-white'}` }/>
+            Home
+          </Link>
+        </li>
+        <li className='text-white col-start-4 flex flex-col items-center'>
+          <Link to="/Service" className='hover:text-blue-300 transition duration-500'>
+            <img src={tech} alt='Service' style={imageStyle} className={({isActive})=> `${isActive ? 'text-blue-300' : 'text-white'}` }/>
+            Service
+          </Link>
+        </li>
+        <li className='text-white col-start-5 flex flex-col items-center'>
+          <Link to="/Projects" className='hover:text-blue-300 transition duration-500'>
+            <img src={book} alt='Projects' style={imageStyle} className={({isActive})=> `${isActive ? 'text-blue-300' : 'text-white'}` }/>
+            Projects
+          </Link>
+        </li>
+        
+      </ul>
+    </div>
   );
 }
 
