95ae9dca3f95213ff5ee729a9eddd3e77845745a
akshatgg <akshatg9636@gmail.com>
2023-12-25 11:09:39 +0545

    adding project page

--- welcome/src/Execute.jsx
+++ welcome/src/Execute.jsx
@@ -1,10 +1,14 @@
 import Home from './components/Home/Home'
 import About from './components/About/About'
+import Project from './components/Projects/Project'
+
+
 function Execute() {
   return (
     <div>
       <Home/>
       <About/>
+      <Project/>
     </div>
   )
 }
---
+++ welcome/src/components/Projects/Project.jsx
@@ -0,0 +1,10 @@
+
+function Project() {
+  return (
+    <div className="h-screen bg-[#040404]">
+      
+    </div>
+  )
+}
+
+export default Project
