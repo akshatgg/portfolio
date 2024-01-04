f6adf98cbb16ffeb7148859e05486f2595094645
akshatgg <akshatg9636@gmail.com>
2024-01-05 00:57:09 +0545

    contact page is ready to send mail

--- welcome/src/Execute.jsx
+++ welcome/src/Execute.jsx
@@ -6,15 +6,17 @@ import { useMediaQuery } from 'react-responsive';
 
 function Execute() {
   // Set up media queries
+  
   const isMobile = useMediaQuery({ maxWidth: 767 }); // Adjust the maximum width as needed
 
   return (
     <div className='overflow-x-hidden'>
       {isMobile ? (
         // Render mobile-specific component or content
-        <div className='text-[100px] text-white'>
+        <div className='text-[50px] text-white'>
           {/* Mobile-specific content goes here */}
-          <p>Only For PC Version</p>
+          <p>Only For PC Setup</p>
+          <p>Mobile view is in progress</p>
         </div>
       ) : (
         // Render desktop-specific components or content
--- welcome/src/components/Contact/Contact.jsx
+++ welcome/src/components/Contact/Contact.jsx
@@ -1,33 +1,53 @@
 import WorldLottie from "./WorldLottie";
 import  { useEffect} from 'react';
 import "./ContactStyle.css"
+import  { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import AOS from "aos";
 import 'aos/dist/aos.css'
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_o3t5lxg', 'template_fgorclf', form.current, '')
+    .then((result) => {
+      console.log('SUCCESS!',result.text);
+      form.current.reset();
+
+    })
+    .catch((error) => {
+      console.error('Error sending message:', error);
+      // Add code to display an error popup or message
+    });
+  };
+
+
+
+
+
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Your logic to handle form submission (e.g., sending an email) goes here
-  };
+
   return (
     <div className="h-screen bg-[#040404] flex items-center justify-center ">
     <div className= "text-white bg-[#100D25] p-11 rounded-3xl ml-[300px]  " data-aos="fade-right">
-          <form onSubmit={handleSubmit} className="">
+          <form ref={form} onSubmit={sendEmail}>
             <p>Get in Touch</p>
              <p className="text-white text-[50px] font-extrabold mb-[15%] ">Contact me</p>
             <label htmlFor="name">Name</label><br />
-            <input type="text" id="name" name="name" placeholder="Name" className="input h-10 rounded bg-[#151030]" />
+            <input type="text" id="name" name="user_name" placeholder="Name" className="input h-10 rounded bg-[#151030]" />
             <br /><br /><br />
 
             <label htmlFor="email">Email</label><span> (required)</span><br />
-            <input type="text" id="email" name="email" placeholder="Email" className="input h-10  rounded bg-[#151030]" required />
+            <input type="text" id="email" name="user_email" placeholder="Email" className="input h-10  rounded bg-[#151030]" required />
             <br /><br />
 
             <br /><label htmlFor="phone">Phone</label><br />
-            <input type="text" id="phone" name="phone" placeholder="Phone Number" className="input h-10 bg-[#151030] rounded" />
+            <input type="text" id="phone" name="user_number" placeholder="Phone Number" className="input h-10 bg-[#151030] rounded" />
             <br /><br />
 
             <br /><label htmlFor="subject">Subject</label><span> (required)</span><br />
@@ -38,7 +58,7 @@ function Contact() {
             <textarea id="message" name="message" rows="4" placeholder="Enter your message" className="input bg-[#151030] rounded"></textarea>
             <br /><br />
 
-            <button type="submit" className="bg-[#151030]" id="mailButton">Send</button>
+            <button type="submit" onClick={sendEmail}  className="bg-[#151030]" id="mailButton">Send</button>
           </form>
         </div>
         <div className="cols-start-2 ml-[200px] " data-aos="fade-left">
