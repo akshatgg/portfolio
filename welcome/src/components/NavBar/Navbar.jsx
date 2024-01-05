34f0fb694865c6441377147dfcea108eb6ce6905
akshatgg <akshatg9636@gmail.com>
2024-01-05 23:10:10 +0545

    Routing in progress

--- welcome/src/components/NavBar/Navbar.jsx
+++ welcome/src/components/NavBar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
 import profile from '../../assets/profile.png';
 import contact from '../../assets/contact.png';
 import book from '../../assets/book.png';
@@ -38,31 +38,46 @@ function Navbar() {
     <div className={`flex justify-center z-10 ${fix ? 'fixed top-0 left-0 w-full bg-black transition duration-1000' : ''}`}>
       <ul className="grid grid-cols-5 gap-11 mt-4">
         <li className='text-white flex flex-col items-center'>
-          <Link to="/About" className='hover:text-blue-300 transition duration-500'>
+          <Link to="/About" className='hover:text-blue-300 transition duration-500  spy={true} 
+      smooth={true} 
+      offset={-100} 
+      duration={500} '>
             <img src={profile} alt='About' style={imageStyle} />
             About
           </Link>
         </li>
         <li className='text-white flex flex-col items-center'>
-          <Link to="/Contact" className='hover:text-blue-300 transition duration-500'>
+          <Link to="/Contact" className='hover:text-blue-300 transition duration-500  spy={true} 
+      smooth={true} 
+      offset={-100} 
+      duration={500} '>
             <img src={contact} alt='Contact' style={imageStyle} />
             Contact
           </Link>
         </li>
         <li className='text-white flex flex-col items-center'>
-          <Link to="/" className='hover:text-blue-300 transition duration-500'>
+          <Link to="/" className='hover:text-blue-300 transition duration-500  spy={true} 
+      smooth={true} 
+      offset={-100} 
+      duration={500} '>
             <img src={home} alt='Home' style={imageStyle} />
             Home
           </Link>
         </li>
         <li className='text-white flex flex-col items-center'>
-          <Link to="/Service" className='hover:text-blue-300 transition duration-500'>
+          <Link to="/Service" className='hover:text-blue-300 transition duration-500  spy={true} 
+      smooth={true} 
+      offset={-100} 
+      duration={500} '>
             <img src={tech} alt='Service' style={imageStyle} />
             Service
           </Link>
         </li>
         <li className='text-white flex flex-col items-center'>
-          <Link to="/Projects" className='hover:text-blue-300 transition duration-500'>
+          <Link to="/Project" className='hover:text-blue-300 transition duration-500  spy={true} 
+      smooth={true} 
+      offset={-100} 
+      duration={500} '>
             <img src={book} alt='Projects' style={imageStyle} />
             Projects
           </Link>
--- welcome/src/main.jsx
+++ welcome/src/main.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './components/Home/Home.jsx'
-const router=(
-  <Router>
-    <Routes>
-       <Route path='/' element={<App/>}/>
-       {/* <Route path='' element={<Home/>}/> */}
-       
-    
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import About from './components/About/About.jsx';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Contact from './components/Contact/Contact.jsx';
+import Project from './components/Projects/Project.jsx';
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [pathname]);
+
+  return null; // React requires a return value for a functional component
+};
+
+const router = (
+  <Router>
+    <ScrollToTop /> {/* Include ScrollToTop component inside the Router */}
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/About' element={<About />} />
+      <Route path='/Contact' element={<Contact />} />
+      <Route path='/Project' element={<Project />} />
     </Routes>
   </Router>
-)
-
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode>{router}</React.StrictMode>);
